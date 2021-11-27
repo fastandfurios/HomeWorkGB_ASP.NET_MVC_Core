8058c34eb76da14cba91d406d64501878a31ec2e
Дмитрий <semu-dima@yandex.ru>
2021-11-27 17:34:33 +0300

    добавил решение первой задачи

--- Lesson1/MainWindow.xaml.cs
+++ Lesson1/MainWindow.xaml.cs
@@ -2,19 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Lesson1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private IList<long> _numbers = new List<long>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(() => CountFibbonaci());
+            thread.Start();
+        }
+
+        private void CountFibbonaci()
+        {
+            foreach (var item in CalulateData())
+            {
+                Numbers.Dispatcher.Invoke(() =>
+                {
+                    Numbers.Text += $" {item}";
+                    Thread.Sleep(1000);
+                }, DispatcherPriority.Background);
+            }
+        }
+
+        private IEnumerable<string> CalulateData()
+        {
+            for (int i = 0; ; i++)
+            {
+                if (i is 0 or 1)
+                {
+                    _numbers.Add(i);
+                    yield return i.ToString();
+                }
+                else
+                {
+                    _numbers.Add(_numbers[i - 2] + _numbers[i - 1]);
+                    yield return (_numbers[i - 2] + _numbers[i - 1]).ToString();
+                }
+            }
         }
     }
 }
