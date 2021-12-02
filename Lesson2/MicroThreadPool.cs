80636694004435f65bd77459ea2be36aa9c89c8e
Дмитрий <semu-dima@yandex.ru>
2021-12-02 07:43:23 +0300

    добавил класс MicroThreadPool

---
+++ Lesson2/MicroThreadPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    public static class MicroThreadPool
+    {
+        private static readonly Queue<Task> _tasks = new();
+
+        public static Task GetTask(Action action)
+        {
+            if(_tasks.Count > 0) return _tasks.Dequeue();
+            return new(action);
+        }
+
+        public static void ReleaseTask(Task task)
+        {
+            if (task is null) return;
+
+            _tasks.Enqueue(task);
+        }
+    }
+}
