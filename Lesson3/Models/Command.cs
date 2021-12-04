cce234fe22ba6c1b9f7b62ac42c2dfc5cbed9b01
Дмитрий <semu-dima@yandex.ru>
2021-12-04 22:04:40 +0300

    добавил интерфейс ICommand и класс Command

---
+++ Lesson3/Models/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Lesson3.Models
+{
+    internal sealed class Command : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public Command(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter)
+            => _canExecute(parameter!);
+
+        public void Execute(object? parameter)
+            => _execute(parameter!);
+    }
+}
---
+++ Lesson3/Models/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson3.Models
+{
+    public interface ICommand
+    {
+        bool CanExecute(object? parameter);
+        void Execute(object? parameter);
+
+        event EventHandler? CanExecuteChanged;
+    }
+}
