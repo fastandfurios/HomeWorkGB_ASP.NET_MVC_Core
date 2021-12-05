8f4100ad02d106c4cd52aa3b4868979e67fc80a5
Дмитрий <semu-dima@yandex.ru>
2021-12-05 16:18:29 +0300

    переименовал класс

---
+++ Lesson3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Lesson3.Models;
+
+namespace Lesson3.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private ICommand? _command;
+        private string? _text;
+
+        public ICommand? Command 
+        { 
+            get
+            {
+                if(_command is null) _command = new Command(obj => ChangeText());
+                return _command;
+            }
+        }
+
+        public string Text 
+        { 
+            get => _text!; 
+            set => SetField(ref _text, value); 
+        }
+
+        private void ChangeText() => _text = "Hello World";
+    }
+}
