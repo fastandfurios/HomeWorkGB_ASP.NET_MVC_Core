42e17310b77e3976e9e3d29679aeef6f94128869
Дмитрий <semu-dima@yandex.ru>
2021-12-05 19:40:04 +0300

    добавление нкоторых свойств

--- Lesson3/ViewModels/MainWindowViewModel.cs
+++ Lesson3/ViewModels/MainWindowViewModel.cs
@@ -4,16 +4,30 @@ namespace Lesson3.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
-        private ICustomCommand? _command;
-        private string? _text = "Hello World";
+        private IClickCommand? _command;
+        private string? _description = "Click Me";
+        private string? _name = "Click";
+        private string? _text;
 
-        public ICustomCommand? Command 
-        { 
+        public IClickCommand? Command
+        {
             get
             {
-                if(_command is null) _command = new CustomCommand(obj => ChangeText());
+                if (_command is null) _command = new ClickCommand(obj => ChangeText());
                 return _command;
             }
+        }
+
+        public string? Description
+        {
+            get => _description;
+            set => SetField(ref _description, value);
+        }
+
+        public string? Name 
+        { 
+            get => _name; 
+            set => SetField(ref _name, value); 
         }
 
         public string Text 
@@ -22,6 +36,10 @@ namespace Lesson3.ViewModels
             set => SetField(ref _text, value); 
         }
 
-        private void ChangeText() => _text = "Hello World";
+        private void ChangeText() 
+        { 
+            Text = "Hello World";
+            Name = "Chanel";
+        }
     }
 }
