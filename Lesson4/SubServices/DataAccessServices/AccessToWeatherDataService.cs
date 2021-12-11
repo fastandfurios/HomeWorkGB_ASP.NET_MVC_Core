4b5783cdf67259e361918f9e90ae70ad31249a2d
Дмитрий <semu-dima@yandex.ru>
2021-12-11 23:08:02 +0300

    сделан сервис доступа к данным

---
+++ Lesson4/SubServices/DataAccessServices/AccessToWeatherDataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson4.SubServices.DataAccessServices
+{
+    public sealed class AccessToWeatherDataService : IAccessToWeatherDataService
+    {
+        private static readonly HttpClient _httpClient = new();
+        private Uri? _url;
+
+        public Uri URL { get => _url!; set => _url = value; }
+
+        public async Task<string> GetResponse()
+        {
+            try
+            {
+                return await _httpClient.GetStringAsync(_url).ConfigureAwait(false);
+            }
+            catch (InvalidOperationException)
+            {
+                throw;
+            }
+            catch (HttpRequestException)
+            {
+                throw;
+            }
+            catch (TaskCanceledException)
+            {
+                throw;
+            }
+        }
+    }
+}
