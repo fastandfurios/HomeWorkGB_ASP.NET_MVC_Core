46a0ebeceba716ab29b7957faafea3896debf9be
Дмитрий <semu-dima@yandex.ru>
2021-12-11 23:09:37 +0300

    добавил синглтон

---
+++ Lesson4/ISingleton.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson4
+{
+    public interface ISingleton<T> where T : class, new()
+    {
+        static object _lockObject = new();
+        static volatile T? _instance;
+        static T? Instance
+        {
+            get
+            {
+                if(_instance is null)
+                {
+                    lock (_lockObject)
+                    {
+                        if(_instance is null) _instance = new T();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
