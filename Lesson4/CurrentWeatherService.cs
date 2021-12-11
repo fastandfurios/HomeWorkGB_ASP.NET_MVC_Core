3c6b60cb083109c1753fda2573be0abdd596c741
Дмитрий <semu-dima@yandex.ru>
2021-12-11 23:09:16 +0300

    добавил основной сервис (фасад)

---
+++ Lesson4/CurrentWeatherService.cs
@@ -0,0 +1,22 @@
+﻿using Lesson4.SubServices.DataAccessServices;
+
+namespace Lesson4
+{
+    public class CurrentWeatherService
+    {
+        private readonly IAccessToWeatherDataService _access;
+
+        public CurrentWeatherService(IAccessToWeatherDataService access)
+        {
+            _access = access;
+        }
+
+        public void Run() => RunServices().ConfigureAwait(false);
+
+        private async Task RunServices()
+        {
+           _access.URL = new("https://www.gismeteo.ru/weather-cherepovets-4285/");
+           var responce = await _access.GetResponse().ConfigureAwait(false);
+        }
+    }
+}
