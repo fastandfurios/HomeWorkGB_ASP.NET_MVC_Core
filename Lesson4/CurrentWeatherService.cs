a9104f42f24f64122a8c6e05522a50fc3e27cd0c
Дмитрий <semu-dima@yandex.ru>
2021-12-12 23:59:33 +0300

    изменения в коде

--- Lesson4/CurrentWeatherService.cs
+++ Lesson4/CurrentWeatherService.cs
@@ -1,22 +1,26 @@
 ﻿using Lesson4.SubServices.DataAccessServices;
+using Lesson4.SubServices.DataExtractionServices;
 
 namespace Lesson4
 {
     public class CurrentWeatherService
     {
-        private readonly IAccessToWeatherDataService _access;
+        private readonly IAccessToWeatherDataService _accessService;
+        private readonly IDataExtractionService _extractionService;
 
-        public CurrentWeatherService(IAccessToWeatherDataService access)
+        public CurrentWeatherService(IAccessToWeatherDataService access, IDataExtractionService extractionService)
         {
-            _access = access;
+            _accessService = access;
+            _extractionService = extractionService;
         }
 
         public void Run() => RunServices().ConfigureAwait(false);
 
         private async Task RunServices()
         {
-           _access.URL = new("https://www.gismeteo.ru/weather-cherepovets-4285/");
-           var responce = await _access.GetResponse().ConfigureAwait(false);
+            _accessService.URL = new("https://www.gismeteo.ru/weather-cherepovets-4285/");
+            var responce = await _accessService.GetResponse().ConfigureAwait(false);
+            _extractionService.GetWeather(responce);
         }
     }
 }
--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 using Lesson4;
 using Lesson4.SubServices.DataAccessServices;
+using Lesson4.SubServices.DataExtractionServices;
 
-var accessSubService = ISingleton<AccessToWeatherDataService>.Instance;
+var accessDataSubService = ISingleton<AccessToWeatherDataService>.Instance;
+var dataExtractionService = ISingleton<DataExtractionService>.Instance;
 
-var currentWeatherService = new CurrentWeatherService(accessSubService!);
+var currentWeatherService = new CurrentWeatherService(accessDataSubService!, dataExtractionService!);
 currentWeatherService.Run();
 
 Console.ReadLine();
