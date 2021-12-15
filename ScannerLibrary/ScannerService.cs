a1337960cb28f518449c720ab5294367d3615e67
Дмитрий <semu-dima@yandex.ru>
2021-12-15 12:10:39 +0300

    добавил сервис сканера

---
+++ ScannerLibrary/ScannerService.cs
@@ -0,0 +1,39 @@
+﻿using ScannerLibrary.Interfaces;
+
+namespace ScannerLibrary
+{
+    public class ScannerService : IScannerService
+    {
+        public async Task<byte[]> ScanAsync(string fileName)
+        {
+            try
+            {
+                byte[] buffer;
+
+                await using var fileStream = File.OpenRead(fileName);
+
+                buffer = new byte[fileStream.Length];
+
+                await fileStream.ReadAsync(buffer: buffer, offset: 0, count: (int)fileStream.Length)
+                                .ConfigureAwait(false);
+                return buffer;
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                throw;
+            }
+            catch (NotSupportedException)
+            {
+                throw;
+            }
+        }
+    }
+}
