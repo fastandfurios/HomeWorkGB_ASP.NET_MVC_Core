2ffdcb84609aa52344911fa9d341b0eae7acdc70
Дмитрий <semu-dima@yandex.ru>
2021-12-15 12:11:08 +0300

    добавил сервис менеджера сканера

---
+++ ManagerScanerLibrary/Interfaces/IManagerScannerService.cs
@@ -0,0 +1,9 @@
+﻿namespace ManagerScannerLibrary.Interfaces
+{
+    public interface IManagerScannerService
+    {
+        Task<bool> SaveToJson(byte[] buffer);
+        Task<bool> SaveToTxt(byte[] buffer);
+        Task<bool> SaveToXml(byte[] buffer);
+    }
+}
---
+++ ManagerScanerLibrary/ManagerScannerService.cs
@@ -0,0 +1,22 @@
+﻿using ManagerScannerLibrary.Interfaces;
+
+namespace ManagerScannerLibrary
+{
+    public class ManagerScannerService : IManagerScannerService
+    {
+        public Task<bool> SaveToJson(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveToTxt(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveToXml(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
