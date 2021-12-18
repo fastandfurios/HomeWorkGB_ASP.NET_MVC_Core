67c5b9bdb787d755def491e582f36d0be0094406
Дмитрий <semu-dima@yandex.ru>
2021-12-18 16:17:30 +0300

    добавил форматы сохранения отсканированного файла

---
+++ ManagerScanerLibrary/XmlFormat.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+using ManagerScannerLibrary.Interfaces;
+using ScannerLibrary.Interfaces;
+
+namespace ManagerScannerLibrary
+{
+    public class XmlFormat : IFormatScan
+    {
+        public async Task ScanAndSave(IScannerService device, string sourceFileName, string targetFileName)
+        {
+            await Task.Run(async () =>
+            {
+                await using var fs = File.Create(targetFileName);
+                var xmlSerializer = new XmlSerializer(typeof(byte[]));
+                var buffer = await device.ScanAsync(sourceFileName);
+                xmlSerializer.Serialize(fs, buffer);
+            });
+        }
+    }
+}
