6b9d1591a627a204c52b61c2efec9823574c4211
Дмитрий <semu-dima@yandex.ru>
2021-12-24 13:24:58 +0300

    добавил данные

---
+++ ExampleWebApp/Data/TestData.cs
@@ -0,0 +1,26 @@
+﻿using ExampleWebApp.Models;
+
+namespace ExampleWebApp.Data
+{
+    public class TestData
+    {
+        public ICollection<Department> Departments { get; } = Enumerable.Range(1, 20)
+            .Select(i => new Department
+            {
+                Id = i,
+                Employees = new List<Employee>
+                {
+                    new()
+                    {
+                        Id = i,
+                        Address = $"Адрес-{i}",
+                        LastName = $"Фамилия-{i}",
+                        Name = $"Имя-{i}",
+                        Salary = i,
+                    }
+                },
+                PhoneNumber = $"Телефон-{i}"
+            })
+            .ToList();
+    }
+}
