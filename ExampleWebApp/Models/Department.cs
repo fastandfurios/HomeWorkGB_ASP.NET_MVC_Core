da70b8e4fdfc48fe7c08a6c7bb6c754e74503825
Дмитрий <semu-dima@yandex.ru>
2021-12-24 13:07:46 +0300

    добавил модели

---
+++ ExampleWebApp/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace ExampleWebApp.Models
+{
+    public class Department
+    {
+        public ICollection<Employee> Employees { get; set; }
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ ExampleWebApp/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace ExampleWebApp.Models
+{
+    public class Employee
+    {
+        public string Address { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
