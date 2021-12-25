e43978ec6b25bc739b8801e6e2ef411153782d6f
Дмитрий <semu-dima@yandex.ru>
2021-12-25 19:23:37 +0300

    изменения кода

--- ExampleWebApp/Controllers/DepartmentController.cs
+++ ExampleWebApp/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using ExampleWebApp.Data;
 using ExampleWebApp.Models;
+using ExampleWebApp.Services.Interfaces;
 using ExampleWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,14 +9,14 @@ namespace ExampleWebApp.Controllers
     public class DepartmentController : Controller
     {
         private readonly ICollection<Department> _departments;
-        private ICollection<Employee> _employees;
         private readonly ILogger<DepartmentController> _logger;
-        
-        public DepartmentController(ILogger<DepartmentController> logger)
+        private readonly IEmployeeService _employeeService;
+
+        public DepartmentController(ILogger<DepartmentController> logger, IEmployeeService employeeService)
         {
             _departments = TestData.Departments;
-            _departments.ToList().ForEach(d => { _employees = d.Employees; });
             _logger = logger;
+            _employeeService = employeeService;
         }
 
         public IActionResult Departments()
@@ -23,10 +24,53 @@ namespace ExampleWebApp.Controllers
             return View(_departments);
         }
 
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Address = employee.Address,
+                LastName = employee.LastName,
+                Name = employee.Name,
+                Salary = employee.Salary
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _employeeService.Delete(id);
+            return RedirectToAction("Employees");
+        }
+
         public IActionResult EditEmployee(int id)
         {
-            var employee = _employees.FirstOrDefault(e => e.Id == id);
+            var employee = _employeeService.GetById(id);
 
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Address = employee.Address,
+                LastName = employee.LastName,
+                Name = employee.Name,
+                Salary = employee.Salary
+            };
+
+            return View(model);
+        }
+
+        public IActionResult DetailsEmployee(int id)
+        {
+            var employee = _employeeService.GetById(id);
             if (employee is null)
                 return NotFound();
 
@@ -48,21 +92,24 @@ namespace ExampleWebApp.Controllers
             if (!ModelState.IsValid)
                 return View(model);
 
-            var employee = _employees.FirstOrDefault(e => e.Id == model.Id);
-            if (employee is null)
-                return NotFound();
-
-            employee.Address = model.Address;
-            employee.LastName = model.LastName;
-            employee.Name = model.Name;
-            employee.Salary = model.Salary;
+            var employee = new Employee
+            {
+                Id = model.Id,
+                Address = model.Address,
+                LastName = model.LastName,
+                Name = model.Name,
+                Salary = model.Salary
+            };
+            
+            _employeeService.Update(employee);
 
             return RedirectToAction(nameof(Employees));
         }
 
         public IActionResult Employees()
         {
-            return View(_employees);
+            var employees = _employeeService.GetAll();
+            return View(employees);
         }
     }
 }
