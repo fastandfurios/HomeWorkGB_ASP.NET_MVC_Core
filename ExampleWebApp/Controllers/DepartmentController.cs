5339de760dabed58f311e85f2bbe0798cb28e53c
Дмитрий <semu-dima@yandex.ru>
2021-12-25 16:22:04 +0300

    добавлено редактирование

--- ExampleWebApp/Controllers/DepartmentController.cs
+++ ExampleWebApp/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using ExampleWebApp.Data;
 using ExampleWebApp.Models;
+using ExampleWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ExampleWebApp.Controllers
@@ -7,20 +8,53 @@ namespace ExampleWebApp.Controllers
     public class DepartmentController : Controller
     {
         private readonly ICollection<Department> _departments;
-        private ICollection<Employee> _employees;
+        private ICollection<Employee> _employees = new List<Employee>();
         private readonly ILogger<DepartmentController> _logger;
         
-
         public DepartmentController(ILogger<DepartmentController> logger)
         {
             _departments = TestData.Departments;
+            _departments.ToList().ForEach(d => { _employees = d.Employees; });
             _logger = logger;
-            TestData.Departments.ToList().ForEach(d => { _employees = d.Employees; });
         }
 
         public IActionResult Departments()
         {
             return View(_departments);
+        }
+
+        public IActionResult EditEmployee(int id)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == id);
+
+            if (employee is null)
+                return NotFound();
+
+            var model = new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Address = employee.Address,
+                LastName = employee.LastName,
+                Name = employee.Name,
+                Salary = employee.Salary
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult EditEmployee(EmployeeViewModel model)
+        {
+            var employee = _employees.FirstOrDefault(e => e.Id == model.Id);
+            if (employee is null)
+                return NotFound();
+
+            employee.Address = model.Address;
+            employee.LastName = model.LastName;
+            employee.Name = model.Name;
+            employee.Salary = model.Salary;
+
+            return RedirectToAction(nameof(Employees));
         }
 
         public IActionResult Employees()
