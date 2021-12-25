b476d19f91ae3de005e9ecd90e42d867a9a28d2a
Дмитрий <semu-dima@yandex.ru>
2021-12-25 19:24:02 +0300

    добавление форм

--- ExampleWebApp/Program.cs
+++ ExampleWebApp/Program.cs
@@ -1,8 +1,12 @@
+using ExampleWebApp.Services;
+using ExampleWebApp.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 services.AddControllersWithViews();
 
+services.AddSingleton<IEmployeeService, EmployeeService>();
 var app = builder.Build();
 
 app.UseStaticFiles();
